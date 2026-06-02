42c23533f548cc45a7da55cb0253c72b870c47e6
NICOLLAS OLIVEIRA MELO <nicollas.melo@a.ucb.br>
2026-06-02 12:08:20 -0300

    feat: rotas http delete e put

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -8,20 +8,43 @@ const register = (req, res) => {
     db.query("insert into user (nome, email) values (?, ?)", [nome, email], 
     (err, result) => {
 
-        if(err) return res.status(500).send(err)
+            if(err) return res.status(500).send(err)
 
-        res.status(201).json({id:result.insertId, nome, email})
-        
-    })
+            res.status(201).json({id:result.insertId, nome, email})
+        })
 }
 
 const list = (req, res) => {
     db.query("select * from user", 
     (err, result) => {
-        if(err) return res.status(500).send(err)
+            if(err) return res.status(500).send(err)
 
-        res.json(result)
-    })
+            res.json(result)
+        })
 }
 
-module.exports = { register, list }
+const hardDelete = (req, res) => {
+    const { id } = req.params
+
+    db.query("delete from user where id = ?", [id],
+    (err, result) => {
+            if(err) return res.status(500).send(err)
+
+            res.sendStatus(204)
+        })
+}
+
+const updateAll = (req, res) => {
+
+    const { nome, email } = req.body
+    const { id } = req.params
+
+    db.query("update user set nome = ?, email = ? where id = ?", [nome, email, id],
+    (err, result) => {
+            if(err) return res.status(500).send(err)
+
+            res.status(201).json({id:result.insertId, nome, email})
+        })
+}
+
+module.exports = { register, list, hardDelete, updateAll }
--- src/routes/api.js
+++ src/routes/api.js
@@ -8,4 +8,8 @@ router.post("/user/register", userController.register)
 
 router.get("/user/list", userController.list)
 
+router.delete("/user/delete/:id", userController.hardDelete)
+
+router.put("/user/updateAll/:id", userController.updateAll)
+
 module.exports = router
